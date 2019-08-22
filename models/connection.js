bfed4e5d1bac57ab43a6fe7e65ee5d3434c17988
Shawn Harris <shawn.harris3510@gmail.com>
2019-08-22 09:50:11 -0400

    add deployment mongo process

--- models/connection.js
+++ models/connection.js
@@ -9,6 +9,23 @@ const mongoose = require('mongoose');
  * This will need to change for every new project you create.
  *
  */
+
+ // Connect to database
+// if (process.env.MONGODB_URI) {
+//   mongoose.connect(process.env.MONGODB_URI);
+// }
+// else {
+//   mongoose.connect('mongodb://localhost/<db-name>');
+// }
+// mongoose.connection.on('error', function(err) {
+//   console.error('MongoDB connection error: ' + err);
+//   process.exit(-1);
+//   }
+// );
+// mongoose.connection.once('open', function() {
+//   console.log("Mongoose has connected to MongoDB!");
+// });
+
 const connectionString = process.env.MONGODB_URI || "mongodb://localhost/<db-name>";
 
 
